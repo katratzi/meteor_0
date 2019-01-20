d69c05e42007bd001490ad3e0a459d267c5103e3
LIANLIQ01\Richard <ric_clifford@hotmail.com>
2019-01-20 19:33:35 +0100

    todo list

--- client/main.js
+++ client/main.js
@@ -1,22 +1,17 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+const todos = [
+  { text: 'pick up milk' },
+  { text: 'say hello world' },
+  { text: 'meeting with boss' },
+];
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.main.helpers({
+  title() {
+    return 'Quick TODOs';
   },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+  todos() {
+    return todos;
+  }
 });
