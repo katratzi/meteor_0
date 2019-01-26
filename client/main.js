8ef82a62b1d2f3be8f4d54e9823da1aaf3915d22
LIANLIQ01\Richard <ric_clifford@hotmail.com>
2019-01-26 19:01:35 +0100

    insert into collections

--- client/main.js
+++ client/main.js
@@ -3,13 +3,31 @@ import { Tasks } from '../imports/collections.js';
 import './main.html';
 
 Template.body.helpers({
-  title() {
-    return 'Quick Tasks';
-  },
+    title() {
+        return 'Quick Tasks';
+    },
 });
 
 Template.body.helpers({
-  tasks() {
-    return Tasks.find({});
-  },
+    tasks() {
+        const tasks = Tasks.find({});
+        return tasks;
+    },
+});
+
+
+Template.body.events({
+    'submit .add-todo'(event) {
+        // don't do a file write
+        event.preventDefault();
+        // get and log text
+        const text = event.target.text.value;        
+        const time = event.target.time.value;        
+        console.log(text + " " + time);
+        // add to db
+        Tasks.insert({text, time})
+        // clear values
+        event.target.text.value ='';        
+        event.target.time.value ='';        
+    }
 });
