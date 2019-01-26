09fa280e32d323f7acd42420c68dacbff45c3652
LIANLIQ01\Richard <ric_clifford@hotmail.com>
2019-01-26 17:15:47 +0100

    closer to tutorial - simpler working Tasks find

--- client/main.js
+++ client/main.js
@@ -1,1 +1,15 @@
-import '../imports/body.js';
+import { Template } from 'meteor/templating';
+import { Tasks } from '../imports/collections.js';
+import './main.html';
+
+Template.body.helpers({
+  title() {
+    return 'Quick Tasks';
+  },
+});
+
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({});
+  },
+});
